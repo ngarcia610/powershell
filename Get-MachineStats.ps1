93aa6589f29d867462b158c8ab2dfa606e019adc
Nate Garcia <nategarcia.va@gmail.com>
2023-12-27 14:32:01 -0500

    Updates readme, adds machinestats.ps1, changes directory structure

---
+++ Get-MachineStats.ps1
@@ -0,0 +1,26 @@
+# Make sure winrm is enabled on the target host
+
+# Ask for a host name
+$server = Read-Host -Prompt "Enter a host name: (Default: localhost)"
+
+# If no value is provided use 'localhost'
+if ($server -eq "") {
+    $server = "localhost"
+}
+
+# A one-line if statement could be as follows
+# $server = ($server -eq "") ? "localhost" : $server
+
+# Gather host info
+$os = Get-CimInstance Win32_OperatingSystem -ComputerName $server
+$memTotal = [math]::Round(($os.TotalVisibleMemorySize / 1MB), 2)
+$memAvailable = [math]::Round(($os.FreePhysicalMemory / 1MB), 2)
+
+# Write out stats
+Write-Host "Stats for $server" -ForegroundColor Green
+Write-Host ('-' * 25)
+Write-Host "Total Memory     : $memTotal GB"
+Write-Host "Available Memory : $memAvailable GB"
+Write-Host "Used Memory      : $($memTotal - $memAvailable) GB"
+Write-Host "Operating System : $($os.Caption)"
+Write-Host "System Drive     : $($os.SystemDrive)\"
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Notes from various PowerShell Courses
+
+## Key Components
+
+- Cmdlet: commands built into the shell
+- Parameter: arguments for cmdlets, functions, scripts
+- Alias: alternative short name for common cmdlets
+- Pipeline: pass objects between cmdlets
+- Function: Commands written in PowerShell
+- Script: PowerShell code stored in .ps1 file
+- Module: package of cmdlets, functions, etc.
+
+## PowerShell Object Members
+
+- Property
+- Method
+- Event
+- AliasProperty
+- NoteProperty
+- ScriptProperty
+- ScriptMethod
+- CodeProperty
+- CodeMethod
+
+## Data Types
+
+- Boolean
+- Integer
+- Decimal
+- String
+- DateTime
+- Array
+- HashTable
+
+## Types of Operators
+
+- Arithmetic
+- Assignment
+- Comparison
+- Logical
+- Redirection
+- Join/Split
+- Type
+- Special
+
