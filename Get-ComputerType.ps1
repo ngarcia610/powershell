e4f1ecab6d4a84c9d1264a130e537c8590b40999
Nate Garcia <nategarcia.va@gmail.com>
2024-02-13 15:21:00 -0500

    Adds comments and deletes old files

--- Get-ComputerType.ps1
+++ Get-ComputerType.ps1
@@ -1,9 +1,12 @@
+# Inventory the machines in a windows domain
+
 $computers = Get-ADComputer -Filter *
 
 foreach ($c in $computers) {
 
   $os = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $c.Name
 
+  # Use product type numbers to determine the type of machine
   switch ($os.ProductType) {
     1 { $type = 'Workstation' }
     2 { $type = 'Domain Controller' }
